d62d06aa81638a8c87d08551d2f3ab22992b0c79
devmateusramos <devmateusramos@gmail.com>
2021-12-06 15:53:19 -0300

    dashboard e summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Headers";
+import { Dashboard } from "./components/Dashboard";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
