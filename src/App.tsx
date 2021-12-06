c7c7aab640e5c6aed7844d51cf6bcee053651128
devmateusramos <devmateusramos@gmail.com>
2021-12-06 15:21:20 -0300

    header feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import styled from "styled-components";
 import { GlobalStyle } from "./styles/global";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
+import { Header } from "./components/Headers";
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello World</Title>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
