1c8fbe32e23afcd79a042310be4a668c4a785ca2
devmateusramos <devmateusramos@gmail.com>
2021-12-10 16:04:37 -0300

    movendo criacao para o context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -14,7 +14,30 @@ interface ITransactionsProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<ITransaction[]>([]);
+// interface ITransactionInput {
+//   title: string;
+//   amount: number;
+//   type: string;
+//   category: string;
+// }
+
+type TransactionInput = Omit<
+  ITransaction,
+  "id" | "createdAt"
+>; /* faz o mesmo que
+ o ITransactionInput comentado -- Ou seja ele pega tudo que é usado
+ no ITransaction menos os que eu passei -- outra opção é utilizar
+ o Pick<ITransaction, 'title' | amount | type | category>
+ nesse caso vai pegar igual do ITransaction os que eu passei. */
+
+interface ITransactionsContextData {
+  transactions: ITransaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<ITransactionsContextData>(
+  {} as ITransactionsContextData
+);
 
 export function TransactionsProvider({ children }: ITransactionsProviderProps) {
   const [transactions, setTransactions] = useState<ITransaction[]>([]);
@@ -25,8 +48,12 @@ export function TransactionsProvider({ children }: ITransactionsProviderProps) {
       .then((response) => setTransactions(response.data.transactions));
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post("/transactions", transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   );
